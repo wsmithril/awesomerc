bdcfb64b3ce136c272a913793bb5b1ccee27bfd8
WsMithril <WsMithril@gmail.com>
2014-04-07 12:37:42 +0800

    Drop lua-lfs dependency in netstats widget

--- mywidgets/utils.lua
+++ mywidgets/utils.lua
@@ -76,4 +76,21 @@ function utils.fstab(dir)
         end})
  end
 
+function utils.split_words(str)
+    local ret = {} 
+    local fpat = "(.-)[:| ]+"
+    local last_end = 1
+    local s, e, cap = str:find(fpat, 1)
+    while s do
+        if s ~= 1 or cap ~= "" then table.insert(ret, cap) end
+        last_end = e + 1
+        s, e, cap = str:find(fpat, last_end)
+    end
+    if last_end <= #str then
+        cap = str:sub(last_end)
+        table.insert(ret, cap)
+    end
+    return ret
+end
+
 return utils
--- mywidgets/widget/netstats.lua
+++ mywidgets/widget/netstats.lua
@@ -1,67 +1,89 @@
--- net stats widgets
+-- net status widgets
 
 -- requires
-local lfs     = require("lfs")
 local wibox   = require("wibox")
 local utils   = require("mywidgets.utils")
 
 -- module
-local widget_netstat = {}
+local M = {}
 
---  Network usage widget {{{
-function net_status()
-    -- return table of network status
-    local base_dir = "/sys/class/net"
+local net_stat = function() 
+    local proc_file = "/proc/net/dev"
     local ret = {}
-    for dev in lfs.dir(base_dir) do
-        if utils.any(function (p) return string.match(dev, p) end, "wlan%d+", "p2p%d+", "eth%d+", "em%d+", "lo") then
-            local fd, status, rx, tx
-            fd = io.open(base_dir .. '/' .. dev .. '/operstate')
-            status = fd:read("*l")
-            fd:close()
-            if status == "up" or dev == "lo" then
-                fd = io.open(base_dir .. '/' .. dev .. '/statistics/rx_bytes')
-                rx = fd:read("*l")
-                fd:close()
-                fd = io.open(base_dir .. '/' .. dev .. '/statistics/tx_bytes')
-                tx = fd:read("*l")
-                fd:close()
-            else
-                rx = 0
-                tx = 0
+    fd = io.open(proc_file)
+    local line_no = 1
+    local idx_recv_bytes = 0
+    local idx_send_bytes = 0
+    
+    for l in fd:lines() do 
+        local fields = utils.split_words(l)
+        if line_no == 1 and not M.fields_prefix then
+            -- order of recieve and transmit
+            M.fields_prefix = {}
+            for _, v in ipairs(fields) do
+                if v == "Receive"  then table.insert(M.fields_prefix, 'rx_') end
+                if v == "Transmit" then table.insert(M.fields_prefix, 'tx_') end
             end
-            ret[dev] = { status = status, rx = tonumber(rx), tx = tonumber(tx) }
+        elseif line_no == 2 and not M.fields then
+            -- get field index we need
+            M.fields = {}
+            local byte_rot = 1
+            local pkt_rot  = 1
+            for i, v in ipairs(fields) do
+                if v == 'bytes' then
+                    M.fields[M.fields_prefix[byte_rot] .. v] = i
+                    byte_rot = byte_rot + 1
+                elseif v == 'packets' then
+                    M.fields[M.fields_prefix[pkt_rot] .. v] = i
+                    pkt_rot = pkt_rot + 1
+                end
+            end
+        elseif line_no > 2 then
+            dev = fields[1]
+            if utils.any(function(p) return string.match(dev, p) end, 'wlan%d+', 'p2p%s+', 'eth%d+', 'em%d+', 'lo') then
+                ret[dev] = {
+                    status     = utils.fstab('/sys/class/net/' .. dev)['operstate'],
+                    rx_bytes   = tonumber(fields[M.fields["rx_bytes"]]   or 0),
+                    tx_bytes   = tonumber(fields[M.fields["tx_bytes"]]   or 0),
+                    rx_packets = tonumber(fields[M.fields["rx_packets"]] or 0),
+                    tx_packets = tonumber(fields[M.fields["tx_packets"]] or 0)} 
+            end
         end
+        line_no = line_no + 1
     end
+    fd:close()
     return ret
 end
 
-function widget_netstat.new(arg)
+function M.new(arg)
     local ret = {}
     ret.widget = wibox.widget.textbox()
     ret.update = arg.update or 3
     local rx_max = (arg.rx_max or 360) * 1024
     local tx_max = (arg.tx_max or 100) * 1024
+    ret.old_status = {}
     ret.updater = function(self)
         local text = ""
-        local line, rx, tx
-        local s = net_status()
-        if self.stats == nil then self.stats = s end
-        for k, v in pairs(s) do
-            if v.status == "up" or k == "lo" then
-                rx = self.stats[k] and (v.rx - self.stats[k].rx) / self.update or 0
-                tx = self.stats[k] and (v.tx - self.stats[k].tx) / self.update or 0
-                line = "✓" .. k .. ' <span color="' .. utils.gradient("#e0e0e0", "#3030EC", 0, tx_max, tx) .. '">' .. utils.format_byte(tx, "sub") .. '</span>'
-                                .. '⇅<span color="' .. utils.gradient("#e0e0e0", "#EC3030", 0, rx_max, rx) .. '">' .. utils.format_byte(rx, "sub") .. '</span>'
-            else
-                line = nil
+        local line, rx, tx, old_status
+        local s = net_stat()
+        for dev, new_status in pairs(s) do
+            if not new_status.status:find('down', 1) then
+
+                old_status = self.old_status[dev]
+                rx_bytes = old_status and (new_status.rx_bytes - old_status.rx_bytes) / self.update or 0
+                tx_bytes = old_status and (new_status.tx_bytes - old_status.tx_bytes) / self.update or 0
+                
+                -- make test for widget
+                line = "✓" .. dev 
+                        .. ' <span color="' .. utils.gradient("#e0e0e0", "#3030EC", 0, tx_max, tx_bytes) .. '">' .. utils.format_byte(tx_bytes, "sub") .. '</span>'
+                        .. '⇅<span color="' .. utils.gradient("#e0e0e0", "#EC3030", 0, rx_max, rx_bytes) .. '">' .. utils.format_byte(rx_bytes, "sub") .. '</span>'
+                if line then text = text .. (text == "" and "" or "|") .. line end
             end
-            if line then text = text .. (text == "" and "" or "|") .. line end
         end
-        self.stats = s
+        self.old_status = s
         self.widget:set_markup(text)
     end
     return ret
 end
 
-return widget_netstat
+return M
